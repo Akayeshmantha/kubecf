3ab99871ffb3f6ec0b211b7bef3cdb36f4c7f714
Akayeshmantha <akayeshmantha@apache.org>
2020-05-06 22:41:03 +0200

    Pushing the release chart.

---
+++ .concourse/tasks/create-pr.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+file_name=$(basename fileName=$(find outputfolder/ -type f -name 'kubecf-v*'));
+branch_name="${file_name%.*}"
+mv outputfolder/kubecf-v* kubecf-master
+
+export GIT_ASKPASS=kubecf-master/.concourse/tasks/git-password.sh
+
+pushd kubecf-helm/
+git checkout -b $branch_name
+git config --global user.name "akayeshmantha"
+git config --global user.email "akayeshmantha@gmail.com"
+git status
+git add $file_name
+git commit -m "bump kubecf helm chart ${file_name%.*}"
+
+git push -f origin $branch_name
+git pull-request --no-fork --title "Update kubecf-helm chart." --message "Increment kubecf helm chart version."
+popd
