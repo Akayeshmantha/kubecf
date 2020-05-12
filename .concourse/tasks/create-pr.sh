62e7014665b59a6e33046b87b2e93651f457db64
Akayeshmantha <akayeshmantha@apache.org>
2020-05-12 10:46:39 +0200

    ok.

--- .concourse/tasks/create-pr.sh
+++ .concourse/tasks/create-pr.sh
@@ -4,16 +4,18 @@ file_name=$(basename fileName=$(find outputfolder/ -type f -name 'kubecf-v*'));
 branch_name="${file_name%.*}"
 mv outputfolder/kubecf-v* kubecf-helm
 
-export GIT_ASKPASS=kubecf-master/.concourse/tasks/git-password.sh
+export GIT_ASKPASS=$git_token
 
 pushd kubecf-helm/
 git checkout -b $branch_name
 git config --global user.name "akayeshmantha"
 git config --global user.email "akayeshmantha@gmail.com"
+git config credential.https://github.com.username akayeshmantha
 git status
 git add $file_name
 git commit -m "bump kubecf helm chart ${file_name%.*}"
+git config --global credential.helper cache
 
 git push -f https://$git_token@github.com/Akayeshmantha/kubecf-helm $branch_name
-git request-pull $branch_name https://$git_token@github.com/Akayeshmantha/kubecf-helm
+git pull-request --no-fork --title "Update kubecf-helm chart." --message "Increment kubecf helm chart version."
 popd
